2e350b1df267434489afc378e0df1192f5000c3a
549049671 <wangxiaodong1021@gmail.com>
2015-07-31 16:42:04 +0800

    修改默认网卡参数为eth0.2

--- src/xd_h3c.c
+++ src/xd_h3c.c
@@ -19,7 +19,7 @@
 
 #define ECHOFLAGS (ECHO | ECHOE | ECHOK | ECHONL)
 
-const char DefaultDevName[] = "eth0";
+const char DefaultDevName[] = "eth0.2";
 char username[100];
 char password[100];
 char devicename[100];
@@ -217,11 +217,11 @@ void print_help()
 	printf("选项:\n");
 	printf("\t-u\t--username\t\t参数为用户名\n"); 
 	printf("\t-p\t--password\t\t参数为密码\n");
-	printf("\t-n\t--device\t\t参数为网卡名，默认为'eth0'\n");
+	printf("\t-n\t--device\t\t参数为网卡名，默认为'eth0.2'\n");
 	printf("\t-h\t--help\t\t\t使用方法\n");
 	printf("\t-l\t--logoff\t\t注销\n");
 	printf("举例:\n");
-	printf("\txdh3c -u abc -p 1234 -n eth0\n");
+	printf("\txdh3c -u abc -p 1234 -n eth0.2\n");
 	printf("\t也可直接使用 xdh3c -u 按照提示输入。\n");
 	printf("\t使用 Ctrl^C 注销登录并退出程序。\n");
 }
@@ -268,7 +268,7 @@ GetPassword:
 void getDevice()
 {
 	char temp[100];
-	printf("请输入网卡名称（默认为eth0）：");
+	printf("请输入网卡名称（默认为eth0.2）：");
 	setbuf(stdin, NULL); //清除缓冲区(Linux),Windows下可以使用fflush或者rewind。
 	fgets(temp,sizeof(char)*100,stdin);
 	if(strlen(temp)==0|| (strlen(temp)==1&&temp[0]=='\n') )
